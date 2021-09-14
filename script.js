0068ec107cd9201a57a1a875fda5aa42c9b1981e
Rogoza Lyuba <lyubashka22rogoza@gmail.com>
2021-09-14 14:55:49 +0300

    finished task2

--- script.js
+++ script.js
@@ -31,15 +31,57 @@ const sumAge = human.reduce((arr,human) => {
 }, 0)
 
 console.log (sumAge);
-// let phones =[];
 
-// function addContact(params) {
-//     let name = document.getElementById("name").value ;
-//     let phone = document.getElementById("phone").value ;
+//====Task2====
 
-//     phones.push(){
-//         name: name,
-//         phone: phone,
+let user0 = {
+    firstName: "Viktor",
+    lastName: "Vlasenko",
+    parentsName: "Nikolaevich",
+    phone: "380948327463",
+};
+let user1 = {
+    firstName: "Vasya",
+    lastName: "Dmitrov",
+    parentsName: "Petrovich",
+    phone: "380992343412",
+};
+let user2 = {
+    firstName: "Victoria",
+    lastName: "Strii",
+    parentsName: "Nikolaevna",
+    phone: "380672303932",
+};
+let user3 = {
+    firstName: "Аlex",
+    lastName: "Serebkin",
+    parentsName: "Albertovich",
+    phone: "380992332434",
+};
+let users = [user0, user1, user2, user3];
 
-//     }
-// }
+//task2 ,A
+
+   const FirsLetterName = users.find(person=>person.firstName[0] === person.lastName[0])
+   console.log(FirsLetterName)
+
+//task2 ,B
+function transformInitials(inValue) {
+    let result = inValue.map(item => [item.lastName, item.firstName[0], item.parentsName[0]].join(" "));
+    
+    return result;
+}
+console.log(transformInitials(users));
+
+//task2 ,C
+
+const serchPhone = users.find(person => person.phone === '380992332434')
+console.log (serchPhone)
+
+//task2 ,D
+function searchPhoneTrue(inValue, phone) {
+    let result = inValue.some(item => item.phone == phone);
+    return result;
+}
+console.log(searchPhoneTrue(users, 380672303932));
+
